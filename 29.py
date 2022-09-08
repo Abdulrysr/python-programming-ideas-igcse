7c4d735992c7ecc5db1045fb44d729691e5e4fdb
Abdulrysr <85631146+Abdulrysr@users.noreply.github.com>
2022-09-08 23:36:35 +0300

    29.py uploaded

--- 29.py
+++ 29.py
@@ -1,6 +1,13 @@
 # 24402 Abdul Rehman Yaser AS PRE ENG B2
 
+"""Write a program that asks a user to enter a number. Use a procedure to output the numbers 1 to the number entered"""
 
-""" Write a program that asks the user to enter a number. Use a procedure to output the numbers 1
-to the number entered """
+def outputNumbers(start, end):
+    for i in range(start, end, 1):
+        print(i)
+
+number = int(input('Enter a number: '))
+outputNumbers(1, number)
+
+
 
